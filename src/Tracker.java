a6b323951c96973cbc560d9c98813b8dd563ac93
Gaspar <rigos.gasparis@gmail.com>
2024-04-10 19:28:02 +0300

    added register,login methods for tracker

--- src/Tracker.java
+++ src/Tracker.java
@@ -1,22 +1,38 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class Tracker{
+public class Tracker extends Thread{
+
+    //Registered Users consists of (Username,Password) tuples
+    private ConcurrentHashMap<String, String> registeredUsers;
+
+    //Active users consists of (tokenID, [IP, port, username]) tuples
+    private ConcurrentHashMap<Integer,String[]> activeUsers;
+
+    //User count statistics consists of (Username, [countDownload,countFail]) tuples
+    private ConcurrentHashMap<String, int[]> userCountStatistics;
+
 
     public Tracker(){
-
+        registeredUsers = new ConcurrentHashMap<>();
+        activeUsers = new ConcurrentHashMap<>();
     }
 
     public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        tracker.start();
+    }
+
+    public void run(){
+
         try {
             ServerSocket server = new ServerSocket(5000);
             while(true){
                 Socket inConnection = server.accept();
-                Thread t = new TrackerThread(inConnection);
+                Thread t = new TrackerThread(inConnection,registeredUsers,activeUsers,userCountStatistics);
                 t.start();
-
-
             }
 
         } catch (IOException e) {
--- src/TrackerThread.java
+++ src/TrackerThread.java
@@ -1,12 +1,146 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TrackerThread extends Thread{
-    @Override
-    public void run() {
-        super.run();
+    private ObjectInputStream in;
+    private Socket connection;
+    private ConcurrentHashMap<String, String> registeredUsers;
+    private ConcurrentHashMap<Integer,String[]> activeUsers;
+    private ConcurrentHashMap<String, int[]> userCountStatistics;
+
+
+    public TrackerThread(Socket connection, ConcurrentHashMap<String, String> registeredUsers, ConcurrentHashMap<Integer,String[]> activeUsers,
+                         ConcurrentHashMap<String, int[]> userCountStatistics) {
+        this.connection = connection;
+        this.registeredUsers = registeredUsers;
+        this.activeUsers = activeUsers;
+        this.userCountStatistics = userCountStatistics;
     }
 
-    public TrackerThread(Socket connection) {
-        //handle connection
+
+
+    @Override
+    public void run() {
+        try {
+            super.run();
+            in = new ObjectInputStream(connection.getInputStream());
+
+            //decide what to do based on the code sent
+            int code = in.readInt();
+
+            switch (code) {
+                case 1:{
+                    registerUser();
+                    break;
+                }
+                case 2:{
+                    loginUser();
+                    break;
+                }
+                case 3:{
+                    logoutUser();
+                }
+
+            }
+
+
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+
+
+    /*
+    ======================================
+
+            CODE BASED FUNCTIONS
+
+    ======================================
+     */
+
+
+    //code = 1
+    //expected input is String, String
+    private void registerUser(){
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
+            String username = (String) in.readObject();
+            if(registeredUsers.containsKey(username)){
+                //user already exists
+                //register failed
+                out.writeInt(0);
+                out.flush();
+                return;
+            }
+            //user does not exist
+            String password = (String) in.readObject();
+            registeredUsers.putIfAbsent(username, password);
+            //register successful
+            out.writeInt(1);
+            out.flush();
+
+
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    //code = 2
+    //expected input is String, String
+    private void loginUser(){
+        try{
+            ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
+            String username = (String) in.readObject();
+            String password = (String) in.readObject();
+
+            if(registeredUsers.containsKey(username) && registeredUsers.get(username).equals(password)){
+                //login successful
+                Random rand = new Random();
+                int sessionID = rand.nextInt(1,10000); //create a pseudorandom id to use for the session
+
+                String[] userDetails = new String[3];
+                userDetails[0] = connection.getInetAddress().getHostAddress(); userDetails[1] = String.valueOf(connection.getLocalPort());; userDetails[2] = username;
+
+                // putIfAbsent will return null if there is no key mapping for the key given, else it returns the previous value which is non-null.
+                // the loop will stop when the key is successfully added to the concurrent hashmap (the return of the method will be null)
+                while(activeUsers.putIfAbsent(sessionID,userDetails)!=null){
+                    sessionID = rand.nextInt(1,10000);
+                }
+
+                //login successful, send session id back to peer.
+                out.writeInt(1);
+                out.writeInt(sessionID);
+                out.flush();
+                return;
+            }
+            //login not successful
+            out.writeInt(0);
+            out.flush();
+
+
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //code = 3
+    //expected input is int
+    private void logoutUser(){
+        try {
+            int tokenID = in.readInt();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
